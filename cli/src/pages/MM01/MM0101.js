fd9e6857fc17c4af2ea7c6b7934a8cc5ec304563
Kim Hanju <khj64076407@gmail.com>
2020-04-01 18:00:39 +0900

    layout

--- cli/src/pages/MM01/MM0101.js
+++ cli/src/pages/MM01/MM0101.js
@@ -1,20 +1,31 @@
 import React from "react";
+import "../../styles/common.css";
+import IconComponent from "../../components/IconComponent";
 
 class MM0101 extends React.Component {
   render() {
     return (
       <div className="mm">
         <div className="mm__header mh">
-          <div className="mh__content"></div>
+          <div className="mh__content">
+            <IconComponent iconName="fas fa-play" />
+            <span>title</span>
+          </div>
         </div>
 
         <div className="mm__content mc">
           <div className="mc__col1">
-            <div className="mc__col1__title">title</div>
+            <div className="mc__col1__title">
+              <IconComponent iconName="fas fa-play" />
+              <span classsName="subTitle">title</span>
+            </div>
             <div className="mc__col1__desc">desc</div>
           </div>
           <div className="mc__col2">
-            <div className="mc__col2__title">title</div>
+            <div className="mc__col2__title">
+              <IconComponent iconName="fas fa-play" />
+              <span classsName="subTitle">title</span>
+            </div>
             <div className="mc__col2__desc">desc</div>
           </div>
         </div>
