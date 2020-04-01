3d67aeaa973ef16aff1e38e52757bdb4b26e13a4
Kim Hanju <khj64076407@gmail.com>
2020-04-01 17:23:27 +0900

    common

--- cli/src/pages/MM01/MM0101.js
+++ cli/src/pages/MM01/MM0101.js
@@ -3,12 +3,12 @@ import React from "react";
 class MM0101 extends React.Component {
   render() {
     return (
-      <div className="mm0101">
-        <div className="mm0101__header mh">
+      <div className="mm">
+        <div className="mm__header mh">
           <div className="mh__content"></div>
         </div>
 
-        <div className="mm0101__content mc">
+        <div className="mm__content mc">
           <div className="mc__col1">
             <div className="mc__col1__title">title</div>
             <div className="mc__col1__desc">desc</div>
