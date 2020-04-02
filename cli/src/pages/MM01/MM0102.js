3d92bec44efcf306a43635c578a7b65d725f12f5
song <songsm5504@gmail.com>
2020-04-02 18:34:53 +0900

    commit

--- cli/src/pages/MM01/MM0102.js
+++ cli/src/pages/MM01/MM0102.js
@@ -1,10 +1,64 @@
 import React from "react";
+import IconComponent from "../../components/IconComponent";
 
 class MM0102 extends React.Component {
   render() {
     return (
-      <div>
-        <h2>연차 관리[MM0102]</h2>
+      <div className="mm">
+        <div className="mm__header mh">
+          <div className="mh__content">
+            <div className="mh__content__title">
+              <IconComponent iconName="fas fa-play" />
+              <span>인사관리 > 연차 관리</span>
+            </div>
+            <div className="mh__content__btn">
+              <span>
+                <input
+                  type="button"
+                  className="btn btn-xs bg-blue"
+                  value="추가"
+                />
+              </span>
+              <span>
+                <input
+                  type="button"
+                  className="btn btn-xs bg-orange"
+                  value="수정"
+                />
+              </span>
+              <span>
+                <input
+                  type="button"
+                  className="btn btn-xs bg-pink"
+                  value="삭제"
+                />
+              </span>
+              <span>
+                <input
+                  type="button"
+                  className="btn btn-xs bg-violet"
+                  value="작성"
+                />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="mm__content mc">
+          <div className="mc__col1">
+            <div className="mc__col1__title">
+              <IconComponent iconName="fas fa-play" />
+              <span className="subTitle">title</span>
+            </div>
+            <div className="mc__col1__desc">desc</div>
+          </div>
+          <div className="mc__col2">
+            <div className="mc__col2__title">
+              <IconComponent iconName="fas fa-play" />
+              <span className="subTitle">title</span>
+            </div>
+            <div className="mc__col2__desc">desc</div>
+          </div>
+        </div>
       </div>
     );
   }
