a426251ac84616e39bb1ad52ad862a75260adb4c
4leaf.ysh <4leaf.ysh@gmail.com>
2020-04-06 11:13:28 +0900

    project

--- cli/src/pages/MM02/MM0202.js
+++ cli/src/pages/MM02/MM0202.js
@@ -1,10 +1,65 @@
 import React from "react";
+import IconComponent from "../../components/IconComponent";
 
 class MM0202 extends React.Component {
   render() {
     return (
-      <div>
-        <h2>진행 프로젝트[MM0202]</h2>
+      <div className="mm">
+        <div className="mm__header mh">
+          <div className="mh__content">
+            <div className="mh__content__title">
+              <IconComponent iconName="fas fa-leaf" />
+              <span>프로젝트 관리 > 진행 프로젝트</span>
+            </div>
+            <div className="mh__content__btn">
+              <span>
+                <input
+                  type="button"
+                  className="btn btn-xs bg-blue"
+                  value="추가"
+                />
+              </span>
+              <span>
+                <input
+                  type="button"
+                  className="btn btn-xs bg-orange"
+                  value="수정"
+                />
+              </span>
+              <span>
+                <input
+                  type="button"
+                  className="btn btn-xs bg-pink"
+                  value="삭제"
+                />
+              </span>
+              <span>
+                <input
+                  type="button"
+                  className="btn btn-xs bg-violet"
+                  value="작성"
+                />
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="mm__content mc">
+          <div className="mc__col1">
+            <div className="mc__col1__title">
+              <IconComponent iconName="fas fa-list-ul" />
+              <span className="subTitle">title</span>
+            </div>
+            <div className="mc__col1__desc">desc</div>
+          </div>
+          <div className="mc__col2">
+            <div className="mc__col2__title">
+              <IconComponent iconName="fas fa-play" />
+              <span className="subTitle">title</span>
+            </div>
+            <div className="mc__col2__desc">desc</div>
+          </div>
+        </div>
       </div>
     );
   }
