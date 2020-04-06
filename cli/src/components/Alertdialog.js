7f6b47fe886798c3909bdb78860c0b183a47b049
njm1128 <njm1128@naver.com>
2020-04-06 22:16:20 +0900

    modified Dialog

--- cli/src/components/Alertdialog.js
+++ cli/src/components/Alertdialog.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import { Alert, AlertTitle } from "@material-ui/lab";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   root: {
