fff1d0fa26310a4cd0e593cd75dc509d128b2613
song <songsm5504@gmail.com>
2020-04-08 13:05:28 +0900

    MM0102

--- cli/src/components/FormDialog.js
+++ cli/src/components/FormDialog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
@@ -6,27 +6,39 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
 
-export default function FormDialog(props) {
-  return (
-    <Dialog
-      open={props.open}
-      aria-labelledby="form-dialog-title"
-      fullWidth={true}
-      maxWidth="md"
-    >
-      <DialogTitle id="form-dialog-title">{props.title}</DialogTitle>
-      <DialogContent>
-        <DialogContentText>{props.content}</DialogContentText>
-        {props.children}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={props.submitDialogHandler} color="primary">
-          등록
-        </Button>
-        <Button onClick={props.closeDialogHandler} color="primary">
-          취소
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+class FormDialog extends React.Component {
+  render() {
+    return (
+      <Dialog
+        open={this.props.open}
+        aria-labelledby="form-dialog-title"
+        fullWidth={true}
+        maxWidth="md"
+      >
+        <DialogTitle id="form-dialog-title">{this.props.title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{this.props.content}</DialogContentText>
+          {this.props.children}
+        </DialogContent>
+        <DialogActions>
+          {this.props.isOnlyCheck ? (
+            <Button onClick={this.props.closeDialogHandler} color="primary">
+              닫기
+            </Button>
+          ) : (
+            <>
+              <Button onClick={this.props.submitDialogHandler} color="primary">
+                {this.props.isModified ? <span>수정</span> : <span>등록</span>}
+              </Button>
+              <Button onClick={this.props.closeDialogHandler} color="primary">
+                취소
+              </Button>
+            </>
+          )}
+        </DialogActions>
+      </Dialog>
+    );
+  }
 }
+
+export default FormDialog;
