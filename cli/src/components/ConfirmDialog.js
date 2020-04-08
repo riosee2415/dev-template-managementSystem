310089b277eca5627f440f6535da69cc840c1932
njm1128 <njm1128@naver.com>
2020-04-08 16:43:46 +0900

    modified Dialog

--- cli/src/components/ConfirmDialog.js
+++ cli/src/components/ConfirmDialog.js
@@ -5,30 +5,44 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 
-export default function ConfirmDialog(props) {
-  return (
-    <Dialog
-      open={props.open}
-      aria-labelledby="confirmation-dialog-title"
-      disableBackdropClick
-      disableEscapeKeyDown
-      fullWidth={true}
-      maxWidth="sm"
-    >
-      <DialogTitle id="confirmation-dialog-title">{props.title}</DialogTitle>
-      <DialogContent dividers>
-        <DialogContentText>{props.content}</DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button autoFocus onClick={props.submitDialogHandler} color="primary">
-          확인
-        </Button>
-        <Button onClick={props.closeDialogHandler} color="primary">
-          취소
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+const styles = {};
+
+class ConfirmDialog extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Dialog
+        open={this.props.isOpen}
+        aria-labelledby="confirmation-dialog-title"
+        disableBackdropClick
+        disableEscapeKeyDown
+        fullWidth={true}
+        maxWidth="sm"
+      >
+        <DialogTitle id="confirmation-dialog-title">
+          {this.props.title}
+        </DialogTitle>
+        <DialogContent dividers>
+          <DialogContentText>{this.props.content}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            autoFocus
+            onClick={this.props.submitDialogHandler}
+            color="primary"
+          >
+            확인
+          </Button>
+          <Button onClick={this.props.closeDialogHandler} color="primary">
+            취소
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
 }
+
+export default withStyles(styles)(ConfirmDialog);
