71c47c32ffc979e3f7e0ec5098d035d97eba4649
Kim Hanju <khj64076407@gmail.com>
2020-04-09 11:38:35 +0900

    icon

--- cli/src/pages/MM01/MM0103.js
+++ cli/src/pages/MM01/MM0103.js
@@ -24,7 +24,7 @@ class MM0103 extends React.Component {
       isConfirmOpen: false,
       confirmTitle: null,
       confirmContent: null,
-      selectedTab: 1
+      selectedTab: 1,
     };
   }
 
@@ -41,7 +41,7 @@ class MM0103 extends React.Component {
       isConfirmOpen,
       confirmTitle,
       confirmContent,
-      selectedTab
+      selectedTab,
     } = this.state;
 
     return (
@@ -201,7 +201,7 @@ class MM0103 extends React.Component {
 
                         <div className="dataBox__row">
                           <span className="data__icon">
-                            <IconComponent iconName="fas fa-map-marker-alt" />
+                            <IconComponent iconName="fas fa-users" />
                           </span>
                           <span className="data__info">
                             {this.state.empInfo.dept}
@@ -298,38 +298,38 @@ class MM0103 extends React.Component {
     );
   }
 
-  _dataClickHandler = async key => {
+  _dataClickHandler = async (key) => {
     const response = await fetch("/api/getEmpInfo", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: JSON.stringify({ key })
+      body: JSON.stringify({ key }),
     });
 
     const data = await response.json();
 
     this.setState({
-      empInfo: data
+      empInfo: data,
     });
   };
 
   _empRegistHandler = async () => {
     this.setState({
-      isEmpRegistFormOpen: true
+      isEmpRegistFormOpen: true,
     });
   };
 
   _empRegistFormSubmitDialogHandler = () => {
     this.setState({
-      isEmpRegistFormOpen: false
+      isEmpRegistFormOpen: false,
     });
   };
 
   _empRegistFormCloseDialogHandler = () => {
     this.setState({
-      isEmpRegistFormOpen: false
+      isEmpRegistFormOpen: false,
     });
   };
 
@@ -341,14 +341,14 @@ class MM0103 extends React.Component {
         isAlertOpen: true,
         alertType: "info",
         alertTitle: "알림",
-        alertContent: "삭제할 직원을 선택해주세요."
+        alertContent: "삭제할 직원을 선택해주세요.",
       });
       return;
     }
     this.setState({
       isConfirmOpen: true,
       confirmTitle: "확인",
-      confirmContent: "[" + empInfo.name + "] 님을 삭제하시겠습니까 ?"
+      confirmContent: "[" + empInfo.name + "] 님을 삭제하시겠습니까 ?",
     });
   };
 
@@ -356,16 +356,16 @@ class MM0103 extends React.Component {
     const { empInfo, isLeftRefresh } = this.state;
 
     this.setState({
-      isConfirmOpen: false
+      isConfirmOpen: false,
     });
 
     const response = await fetch("/api/removeEmpInfo", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: JSON.stringify({ empInfo })
+      body: JSON.stringify({ empInfo }),
     });
 
     const data = await response.json();
@@ -375,19 +375,19 @@ class MM0103 extends React.Component {
         isAlertOpen: true,
         alertType: "success",
         alertTitle: "알림",
-        alertContent: "삭제 처리되었습니다."
+        alertContent: "삭제 처리되었습니다.",
       });
 
       this.setState({
         empInfo: null,
-        isLeftRefresh: !isLeftRefresh
+        isLeftRefresh: !isLeftRefresh,
       });
     } else {
       this.setState({
         isAlertOpen: true,
         alertType: "error",
         alertTitle: "알림",
-        alertContent: "데이터 처리 중 문제가 발생했습니다."
+        alertContent: "데이터 처리 중 문제가 발생했습니다.",
       });
     }
   };
