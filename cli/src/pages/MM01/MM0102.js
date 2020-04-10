ec6914422e83de7903be0edb679a0c7097bd6ec9
song <songsm5504@gmail.com>
2020-04-10 17:28:42 +0900

    MM0102

--- cli/src/pages/MM01/MM0102.js
+++ cli/src/pages/MM01/MM0102.js
@@ -28,7 +28,6 @@ class MM0102 extends React.Component {
       alertContent: null,
       isUsedFormOpen: false,
       isUsageFormOpen: false,
-      allUsageDay: null,
     };
   }
 
@@ -49,7 +48,6 @@ class MM0102 extends React.Component {
       alertContent,
       isUsedFormOpen,
       isUsageFormOpen,
-      allUsageDay,
     } = this.state;
 
     const columns = [
@@ -260,7 +258,7 @@ class MM0102 extends React.Component {
             <>
               <div className="usageSubTitle">
                 <div>{dataInfo.name}</div>
-                <div>{new Date().getFullYear()}</div>
+                <div>신청일 : {dataInfo.currentDate}</div>
               </div>
             </>
           ) : null}
@@ -271,10 +269,10 @@ class MM0102 extends React.Component {
               <span> ~ </span>
               <DatePickers
                 lab="종료일"
-                dateId="annuaEndDay"
+                dateId="annualEndDay"
                 changed={this._getUseDay}
               />
-              <div>(총 : 일)</div>
+              <div id="allusageDay-js">(총 : 일)</div>
             </div>
           </div>
 
@@ -282,17 +280,18 @@ class MM0102 extends React.Component {
             <span>사유 : </span>
             <TextField
               id="applicationReason-js"
+              multiline
               margin="dense"
               type="text"
               label="사유"
-              variant="outlined"
             />
           </div>
 
           <input type="file"></input>
 
           <div className="annualSettlement">
-            결제자 :
+            <div className="personSettlement"> 결제자 :</div>
+
             <ComboBox
               dataList={[{ title: "aaa" }, { title: "bbb" }]}
               label="결제자"
@@ -386,6 +385,19 @@ class MM0102 extends React.Component {
 
     const data = await response.json();
 
+    const date = new Date();
+
+    let y = date.getFullYear();
+    let m = date.getMonth() + 1;
+    let d = date.getDate();
+
+    m = m < 10 ? "0" + m : m;
+    d = d < 10 ? "0" + d : d;
+
+    const appDay = y + "-" + m + "-" + d;
+
+    data.currentDate = appDay;
+
     const workYear = data.hire.substring(0, 4) - new Date().getFullYear() + 1;
     data.hireYear = workYear;
 
@@ -410,12 +422,12 @@ class MM0102 extends React.Component {
 
   _getUseDay = () => {
     const annualStartDay = document.getElementById("annualStartDay");
-    const annuaEndDay = document.getElementById("annuaEndDay");
+    const annualEndDay = document.getElementById("annualEndDay");
 
     let sDay = annualStartDay.value.replace("-", "");
     sDay = sDay.replace("-", "");
 
-    let eDay = annuaEndDay.value.replace("-", "");
+    let eDay = annualEndDay.value.replace("-", "");
     eDay = eDay.replace("-", "");
 
     sDay = parseInt(sDay);
@@ -424,14 +436,18 @@ class MM0102 extends React.Component {
     if (eDay < sDay) {
       alert("종료일이 시작이보다 ");
       return;
-    }
-
-    if (eDay - sDay > 15) {
-      alert("15일 이상은 사용할 수 없습니다.");
+    } else if (eDay - sDay > 15) {
+      alert("15일 이상은 신청할 수 없습니다.");
       return;
     }
 
     const useDay = eDay - sDay + 1;
+
+    const eMs = document.getElementById("allusageDay-js");
+
+    eMs.innerHTML = useDay;
+
+    console.log(useDay);
   };
 
   __usageApplicationHandler = () => {
